18ff149e146a0af496e4f890d33df6b725be7c5f
Christopher Wan <christopher.wan@yale.edu>
2015-08-13 16:20:04 -0400

    add functionality for random + number of images

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var phantom = require('phantom');
 
 /* GET home page. */
-router.get('/:search', function(req, res, next) {
+router.get('/top/:search', function(req, res, next) {
 	var urlSearch = req.params.search.split(' ').join('+');
 	var url = 'http://thenounproject.com/search/?q=' + urlSearch;
 	phantom.create(function (ph) {
@@ -25,5 +25,98 @@ router.get('/:search', function(req, res, next) {
 		});
 	});
 });
+
+router.get('/top/:search/:number', function(req, res, next) {
+	var urlSearch = req.params.search.split(' ').join('+');
+	var url =  'http://thenounproject.com/search/?q=' + urlSearch;
+	var number = parseInt(req.params.number);
+	phantom.create(function (ph) {
+		ph.createPage(function (page) {
+			page.open(url, function (status) {
+				page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js", function() {
+					page.evaluate(function(number) {
+						var imagesArray = [];
+						var images = $('.Grid-cell');
+						var iterator = Math.min(number, images.length);
+						for(i = 0; i < iterator; i++) {
+							imagesArray.push($($(images).get(i)).find('img').attr('src'));
+						}
+						return imagesArray;
+					}, function (result) {
+						var object = {
+							"query": req.params.search,
+							"images": result
+						};
+						res.send(object);
+					}, number);
+				});
+			});
+		});
+	});
+});
+
+router.get('/random/:search', function(req, res, next) {
+	var urlSearch = req.params.search.split(' ').join('+');
+	var url =  'http://thenounproject.com/search/?q=' + urlSearch;
+	phantom.create(function (ph) {
+		ph.createPage(function (page) {
+			page.open(url, function (status) {
+				page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js", function() {
+					page.evaluate(function() {
+						var images = $('.Grid-cell');
+						var random = Math.floor(Math.random() * images.length);
+						var image = $($(images).get(random)).find('img').attr('src');
+						return image;
+					}, function (result) {
+						var object = {
+							"query": req.params.search,
+							"image": result
+						};
+						res.send(object);
+					});
+				});
+			});
+		});
+	});
+});
+
+router.get('/random/:search/:number', function(req, res, next) {
+	var urlSearch = req.params.search.split(' ').join('+');
+	var url =  'http://thenounproject.com/search/?q=' + urlSearch;
+	var number = parseInt(req.params.number);
+	phantom.create(function (ph) {
+		ph.createPage(function (page) {
+			page.open(url, function (status) {
+				page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js", function() {
+					page.evaluate(function(number) {
+						var imagesArray = [];
+						var images = $('.Grid-cell');
+						var iterator = Math.min(number, images.length);
+						var used = [];
+						for(i = 0; i < iterator; i++) {
+							var random = Math.floor(Math.random() * images.length);
+							while(used.indexOf(random) !== -1) {
+								random = Math.floor(Math.random() * images.length);
+							}
+							used.push(random);
+							imagesArray.push($($(images).get(random)).find('img').attr('src'));
+						}
+						return imagesArray;
+					}, function (result) {
+						var object = {
+							"query": req.params.search,
+							"images": result
+						};
+						res.send(object);
+					}, number);
+				});
+			});
+		});
+	});
+});
+
+
+
+
 
 module.exports = router;
